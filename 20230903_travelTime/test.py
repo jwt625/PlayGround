53984e6b7c6c3e0fd7331f1d8c79ab8b705cd466
Wentao <wentao@stanford.edu>
2023-09-03 12:03:50 -0700

    Update test.py
    Finally got it working

--- 20230903_travelTime/test.py
+++ 20230903_travelTime/test.py
@@ -5,6 +5,11 @@ from datetime import datetime
 import json
 
 
+# have to use this to get rid of "RuntimeError: asyncio.run() cannot be called from a running event loop"
+import nest_asyncio
+
+nest_asyncio.apply() 
+
 #%%
 with open('apikey.json', 'r') as json_file:
     data = json.load(json_file)
@@ -13,7 +18,7 @@ with open('apikey.json', 'r') as json_file:
 from traveltimepy import Driving, Coordinates, TravelTimeSdk
 
 async def main():
-    sdk = TravelTimeSdk(data.APP_ID, data.APP_KEY)
+    sdk = TravelTimeSdk(data['APP_ID'], data['APP_KEY'])
     
     results = await sdk.time_map_async(
         coordinates=[Coordinates(lat=51.507609, lng=-0.128315), Coordinates(lat=51.517609, lng=-0.138315)],
