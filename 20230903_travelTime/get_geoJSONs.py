3af7afd9c15c8a811bf302a4aa49ff3a2c8fd3b1
Wentao <wentao@stanford.edu>
2023-09-03 20:03:32 -0700

    add different drive time

--- 20230903_travelTime/get_geoJSONs.py
+++ 20230903_travelTime/get_geoJSONs.py
@@ -11,10 +11,15 @@ df = pd.read_csv(file_path, header=None, names=['lat', 'lng'])
 from helpers_TT import get_geoJSON
 import time
 
-for ii in range(len(df.lat)):
-    ii
-    get_geoJSON(lat = df.lat[ii], lng = df.lng[ii], filename=f"{ii}.geojson")
-    time.sleep(1)
+# t_drive = 3600+1800
+for t_drive in [3600, 5400, 7200]:
+    # for ii in range(len(df.lat)):
+    for ii in [len(df.lat)-1]:    # only adding the last one
+        ii
+        get_geoJSON(lat = df.lat[ii], lng = df.lng[ii],
+                    filename=f"{t_drive}_{ii}.geojson", travel_time=t_drive
+                    )
+        time.sleep(1)
 
 
 # %%
--- 20230903_travelTime/plot_travel_time.py
+++ 20230903_travelTime/plot_travel_time.py
@@ -23,13 +23,16 @@ def random_color(feature):
 
 icon_house = AwesomeIcon(name='home', marker_color='green')
 
+t_drive=5400
+
 for ii in range(len(df.lat)):
-    with open(f'{ii}.geojson', 'r') as json_file:
+    
+    with open(f'{t_drive}_{ii}.geojson', 'r') as json_file:
         data = json.load(json_file)
     geo_json = GeoJSON(
         data=data,
         style={
-            'opacity': 1, 'dashArray': '9', 'fillOpacity': 0.1, 'weight': 1
+            'opacity': 1, 'dashArray': '9', 'fillOpacity': 0.2, 'weight': 1
         },
         hover_style={
             'color': 'white', 'dashArray': '0', 'fillOpacity': 0.5
@@ -37,6 +40,8 @@ for ii in range(len(df.lat)):
         style_callback=random_color
     )
     m.add_layer(geo_json)
+
+
     marker = Marker(location=(df.lat[ii], df.lng[ii]), draggable=False, icon=icon_house)
     m.add_layer(marker)
 
