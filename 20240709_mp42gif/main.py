de88e85a5f4814475ab3003854cb7c75184ef5cb
Wentao <wentao@stanford.edu>
2024-07-19 21:52:54 -0700

    update mp4 gif converter

--- 20240709_mp42gif/main.py
+++ 20240709_mp42gif/main.py
@@ -18,9 +18,15 @@ def convert_mp4_to_gif(mp4_path, gif_path, start_time=0, end_time=None):
     # Write the result to a GIF file
     trimmed_clip.write_gif(gif_path, fps=10)  # You can adjust fps as needed
 
-#%% Example usage
-mp4_path = "Animation_Different Quantities Animation.mp4"
-gif_path = "Animation_Different Quantities Animation.gif"
+# #%% Example usage
+# mp4_path = "Animation_Different Quantities Animation.mp4"
+# gif_path = "Animation_Different Quantities Animation.gif"
+# mp4_path = "This tiny solar-powered flyer weighs less than a paper plane - YouTube - Google Chrome 2024-07-18 20-59-53.mp4"
+# gif_path = "This tiny solar-powered flyer weighs less than a paper plane - YouTube - Google Chrome 2024-07-18 20-59-53.gif"
+mp4_path = "This tiny solar-powered flyer weighs less than a paper plane - YouTube - Google Chrome 2024-07-18 21-00-24.mp4"
+gif_path = "This tiny solar-powered flyer weighs less than a paper plane - YouTube - Google Chrome 2024-07-18 21-00-24.gif"
+
+
 convert_mp4_to_gif(mp4_path, gif_path)
 
 # %%
