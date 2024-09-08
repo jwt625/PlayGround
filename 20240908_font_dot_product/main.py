46535504407b4ea14554f45a97226b328637e995
Wentao <wentao@stanford.edu>
2024-09-08 16:13:11 -0700

    add convolution

--- 20240908_font_dot_product/main.py
+++ 20240908_font_dot_product/main.py
@@ -50,7 +50,6 @@ Image.fromarray(img1).show()
 Image.fromarray(img2).show()
 
 
-
 #%%
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
@@ -62,11 +61,11 @@ def render_letter(letter, font_path, image_size=(100, 100), font_size=80):
     font = ImageFont.truetype(font_path, font_size)
     image = Image.new('L', image_size, color=255)  # 'L' mode for grayscale
     draw = ImageDraw.Draw(image)
-    
+
     # Get text bounding box (coordinates of the rendered text)
     bbox = draw.textbbox((0, 0), letter, font=font)
     w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
-    
+
     # Draw the text centered
     draw.text(((image_size[0] - w) / 2, (image_size[1] - h) / 2), letter, font=font, fill=0)
     return np.array(image)
@@ -77,15 +76,16 @@ def dot_product(img1, img2):
 
 # Function to compute dot products for all letters in a font
 def compute_dot_matrix(font_path):
-    alphabet = string.ascii_uppercase  # List of all uppercase letters
+    # alphabet = string.ascii_uppercase  # List of all uppercase letters
+    alphabet = string.ascii_lowercase  # List of all uppercase letters
     n = len(alphabet)
-    
+
     # Initialize an empty matrix to store dot products
     dot_matrix = np.zeros((n, n))
-    
+
     # Render all letters from the font
     images = {letter: render_letter(letter, font_path) for letter in alphabet}
-    
+
     # Compute dot products between all pairs of letters
     for i, letter1 in enumerate(alphabet):
         for j, letter2 in enumerate(alphabet):
@@ -94,7 +94,7 @@ def compute_dot_matrix(font_path):
             d1 = dot_product(img1, img1)
             d2 = dot_product(img2, img2)
             dot_matrix[i, j] = dot_product(img1, img2)/np.sqrt(d1*d2)
-    
+
     return dot_matrix
 
 # Function to plot the dot product matrix
@@ -116,3 +116,85 @@ dot_matrix = compute_dot_matrix(font_path)
 
 # Plot the dot product matrix
 plot_dot_matrix(dot_matrix, font_path)
+
+
+#%% with convolution
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+import string
+import matplotlib.pyplot as plt
+
+def render_letter(letter, font_path, image_size=(100, 100), font_size=80):
+    font = ImageFont.truetype(font_path, font_size)
+    image = Image.new('L', image_size, color=255)  # 'L' mode for grayscale
+    draw = ImageDraw.Draw(image)
+    
+    # Get text bounding box (coordinates of the rendered text)
+    bbox = draw.textbbox((0, 0), letter, font=font)
+    w, h = bbox[2] - bbox[0], bbox[3] - bbox[1]
+    
+    # Draw the text centered
+    draw.text(((image_size[0] - w) / 2, (image_size[1] - h) / 2), letter, font=font, fill=0)
+    return np.array(image)
+
+def convolve_and_dot(img1, img2):
+    h, w = img1.shape
+    max_shift = h // 5  # Maximum shift is half the image size
+    max_dot = 0
+
+    for y_shift in range(-max_shift, max_shift + 1):
+        for x_shift in range(-max_shift, max_shift + 1):
+            # Shift img2
+            shifted_img2 = np.roll(img2, (y_shift, x_shift), axis=(0, 1))
+            
+            # Calculate dot product
+            dot = np.sum(img1 * shifted_img2)
+            
+            # Update max_dot if necessary
+            max_dot = max(max_dot, dot)
+
+    return max_dot
+
+def compute_dot_matrix(font_path):
+    alphabet = string.ascii_lowercase
+    n = len(alphabet)
+    
+    dot_matrix = np.zeros((n, n))
+    
+    images = {letter: render_letter(letter, font_path) for letter in alphabet}
+    
+    for i, letter1 in enumerate(alphabet):
+        for j, letter2 in enumerate(alphabet):
+            img1 = 1.0 - images[letter1] / 255.0
+            img2 = 1.0 - images[letter2] / 255.0
+            d1 = convolve_and_dot(img1, img1)
+            d2 = convolve_and_dot(img2, img2)
+            dot_matrix[i, j] = convolve_and_dot(img1, img2) / np.sqrt(d1 * d2)
+    
+    return dot_matrix
+
+def plot_dot_matrix(dot_matrix, str_font):
+    plt.figure(figsize=(10, 10))
+    plt.imshow(dot_matrix, cmap='hot', interpolation='nearest')
+    plt.colorbar(label="Normalized Max Dot Product")
+    plt.xticks(ticks=np.arange(26), labels=list(string.ascii_lowercase))
+    plt.yticks(ticks=np.arange(26), labels=list(string.ascii_lowercase))
+    plt.title(f'Max Dot Product Matrix of Letters (with 2D convolution), {str_font}')
+    plt.tight_layout()
+    plt.show()
+
+# Load the font and compute the dot product matrix
+# font_path = "Times New Roman.ttf"
+# font_path = "helvetica.ttf"
+# font_path = "JetBrainsMono-Medium.ttf"
+font_path = "Comic Sans MS.ttf"
+dot_matrix = compute_dot_matrix(font_path)
+
+# Plot the dot product matrix
+plot_dot_matrix(dot_matrix, font_path)
+
+
+
+
+
+#%% with fast convolution
