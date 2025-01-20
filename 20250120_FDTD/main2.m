8dfd18492b164423795123ec0dceb2dfa4375989
Wentao <jwt625@gmail.com>
2025-01-20 12:37:56 -0800

    Update main2.m
    Add plot of final fields

--- 20250120_FDTD/main2.m
+++ 20250120_FDTD/main2.m
@@ -1,4 +1,4 @@
-% 2D FDTD Simulation for TMz Polarization with Improved Visualization
+% 2D FDTD Simulation for TMz Polarization with Final Field Plots and GIF Export
 
 %%
 % Clear workspace and close figures
@@ -9,26 +9,29 @@ Nx = 300;           % Number of cells in x-direction
 Ny = 300;           % Number of cells in y-direction
 dx = 3e-3;          % Spatial step in x (meters)
 dy = 3e-3;          % Spatial step in y (meters)
-dt = dx/(3e8*sqrt(2)); % Time step (Courant condition)
-Nt = 300;           % Number of time steps
+% dt = dx/(3e8*sqrt(2)); % Time step (Courant condition)
+
+Nt = 10;           % Number of time steps
 c0 = 3e8;           % Speed of light in vacuum
 mu0 = pi*4e-7;      % Permeability of free space
 eps0 = 8.85e-12;    % Permittivity of free space
+
+dt = 1/(c0*sqrt(1/dx^2 + 1/dy^2)) * 0.99; % 99% of stability limit
 
 % User-defined parameters
 % 1. Refractive index distribution (Nx x Ny matrix)
 n = ones(Nx, Ny);   % Initialize to vacuum
 % Example: Add a circular obstacle
 % [X, Y] = meshgrid(1:Nx, 1:Ny);
-% n(sqrt((X-50).^2 + (Y-50).^2) < 20) = 2;
+% n(sqrt((X-150).^2 + (Y-150).^2) < 50) = 2;
 
 % 2. Source parameters
 f = 10e9;           % Source frequency (Hz)
 lambda = c0/f;      % Wavelength
 omega = 2*pi*f;     % Angular frequency
 
-% Create vertical line source (~10 wavelengths long)
-source_length = round(3*lambda/dx); % Number of cells for 10 wavelengths
+% Create vertical line source (~3 wavelengths long)
+source_length = round(10*lambda/dx); % Number of cells for 3 wavelengths
 sources = struct('i', {}, 'j', {}, 'A', {}, 'phi', {});
 center_x = round(Nx/2);
 start_y = round(Ny/2 - source_length/2);
@@ -83,12 +86,13 @@ CEy = -dt./(epsilon_Ey * dx);
 CHz_x = dt/(mu0 * dx);
 CHz_y = -dt/(mu0 * dy);
 
-% First pass to determine maximum field values
+% First pass to determine maximum field values and final states
 max_Ex = 1e-9;
 max_Ey = 1e-9;
 max_Hz = 1e-9;
+final_fields = struct();
 
-disp('Calculating field maxima...');
+disp('Calculating field maxima and final states...');
 tic;
 for n = 1:Nt
     % Field updates
@@ -98,12 +102,14 @@ for n = 1:Nt
             Ex(i,j) = Ex(i,j) + CEx(i,j) * (Hz(i,j) - Hz(i,j-1));
         end
     end
+    
     % Update Ey field
     for i = 2:Nx
         for j = 1:Ny
             Ey(i,j) = Ey(i,j) + CEy(i,j) * (Hz(i,j) - Hz(i-1,j));
         end
     end
+    
     % Update Hz field
     for i = 1:Nx
         for j = 1:Ny
@@ -112,6 +118,7 @@ for n = 1:Nt
             Hz(i,j) = Hz(i,j) + CHz_x * dEy_dx + CHz_y * dEx_dy;
         end
     end
+    
     % Apply sources
     for s = 1:length(sources)
         i = sources(s).i;
@@ -130,20 +137,78 @@ for n = 1:Nt
     max_Ex = max(max_Ex, max(abs(Ex_plot(:))));
     max_Ey = max(max_Ey, max(abs(Ey_plot(:))));
     max_Hz = max(max_Hz, max(abs(Hz_plot(:))));
+    
+    % Store final fields
+    if n == Nt
+        final_fields.Ex = Ex_plot;
+        final_fields.Ey = Ey_plot;
+        final_fields.Hz = Hz_plot;
+    end
 end
 toc;
-% Second pass for visualization
+
+% Plot final field distributions
+fig_final = figure('Position', [100 100 1200 400]);
+subplot(1,3,1);
+imagesc(final_fields.Ex');
+clim([-max_Ex max_Ex]);
+title('Final E_x Field');
+axis equal tight off;
+colorbar;
+
+subplot(1,3,2);
+imagesc(final_fields.Ey');
+clim([-max_Ey max_Ey]);
+title('Final E_y Field');
+axis equal tight off;
+colorbar;
+
+subplot(1,3,3);
+imagesc(final_fields.Hz');
+clim([-max_Hz max_Hz]);
+title('Final H_z Field');
+axis equal tight off;
+colorbar;
+
+colormap(jet);
+sgtitle('Final Field Distributions');
+drawnow;
+
+%%
+% Second pass for GIF generation
 Ex = zeros(Nx, Ny+1);
 Ey = zeros(Nx+1, Ny);
 Hz = zeros(Nx, Ny);
 
-fig = figure('Position', [100 100 2000 600]);
+fig_gif = figure('Position', [100 100 2000 600]);
 filename = 'fdtd_3components.gif';
 
-disp('Generating visualization...');
+disp('Generating animation...');
 tic;
 for n = 1:Nt
     % Field updates
+    % Update Ex field
+    for i = 1:Nx
+        for j = 2:Ny
+            Ex(i,j) = Ex(i,j) + CEx(i,j) * (Hz(i,j) - Hz(i,j-1));
+        end
+    end
+    
+    % Update Ey field
+    for i = 2:Nx
+        for j = 1:Ny
+            Ey(i,j) = Ey(i,j) + CEy(i,j) * (Hz(i,j) - Hz(i-1,j));
+        end
+    end
+    
+    % Update Hz field
+    for i = 1:Nx
+        for j = 1:Ny
+            dEy_dx = Ey(i+1,j) - Ey(i,j);
+            dEx_dy = Ex(i,j+1) - Ex(i,j);
+            Hz(i,j) = Hz(i,j) + CHz_x * dEy_dx + CHz_y * dEx_dy;
+        end
+    end
     
     % Apply sources
     for s = 1:length(sources)
@@ -159,34 +224,31 @@ for n = 1:Nt
     Ey_plot = (Ey(1:end-1,:) + Ey(2:end,:))/2;
     Hz_plot = Hz;
     
-    % Plot all three components
+    % Plot all components
+    figure(fig_gif);
     subplot(1,3,1);
     imagesc(Ex_plot');
     clim([-max_Ex max_Ex]);
-    title('E_x Component');
-    axis equal tight;
-    colorbar
+    title(sprintf('E_x @ step %d/%d', n, Nt));
+    axis equal tight off;
     
     subplot(1,3,2);
     imagesc(Ey_plot');
     clim([-max_Ey max_Ey]);
-    title('E_y Component');
-    axis equal tight;
-    colorbar
+    title(sprintf('E_y @ step %d/%d', n, Nt));
+    axis equal tight off;
     
     subplot(1,3,3);
     imagesc(Hz_plot');
     clim([-max_Hz max_Hz]);
-    title('H_z Component');
-    axis equal tight;
-    colorbar
+    title(sprintf('H_z @ step %d/%d', n, Nt));
+    axis equal tight off;
     
     colormap(jet);
-    sgtitle(sprintf('Time Step: %d / %d', n, Nt));
     drawnow;
     
-    % Capture and save frame to GIF
-    frame = getframe(fig);
+    % Export to GIF
+    frame = getframe(fig_gif);
     im = frame2im(frame);
     [imind, cm] = rgb2ind(im, 256);
     if n == 1
@@ -196,3 +258,4 @@ for n = 1:Nt
     end
 end
 toc;
+disp('Simulation complete!');
