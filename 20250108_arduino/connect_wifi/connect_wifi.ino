e5cca6c409245e26a01a4d07a11dc6e297f22059
Wentao <jwt625@gmail.com>
2025-02-05 23:23:12 -0800

    Create connect_wifi.ino

---
+++ 20250108_arduino/connect_wifi/connect_wifi.ino
@@ -0,0 +1,30 @@
+#include "WiFi.h"
+
+// Replace with your network credentials
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println();
+  Serial.println(WiFi.localIP());
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  initWiFi();
+}
+
+void loop() {
+  
+}
