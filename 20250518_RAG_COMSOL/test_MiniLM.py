cf0d5a495ed24154b1bd431df27030a18f34d9b8
Wentao <jwt625@gmail.com>
2025-05-18 23:45:33 -0700

    Update test_MiniLM.py

--- 20250518_RAG_COMSOL/test_MiniLM.py
+++ 20250518_RAG_COMSOL/test_MiniLM.py
@@ -22,3 +22,25 @@ print(similarities.shape)
 from huggingface_hub import snapshot_download
 
 model_path = snapshot_download("sentence-transformers/all-MiniLM-L6-v2", local_dir="./local_models/all-MiniLM-L6-v2", local_dir_use_symlinks=False)
+
+# %%
+
+
+#%%
+from huggingface_hub import HfApi
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv("HUGGINGFACE_TOKEN")
+
+api = HfApi()
+try:
+    user_info = api.whoami(token=token)
+    print("Token is valid! User info:", user_info)
+except Exception as e:
+    print("Token is invalid or expired:", e)
+
+    
+# %%
