d8216e4fa9fb7957b0dd43c32cc5b6f6076a04b3
Wentao <jwt625@gmail.com>
2025-06-01 00:50:01 -0700

    fixed math font

--- 20250531_markXdown/lib/mathjax-config.js
+++ 20250531_markXdown/lib/mathjax-config.js
@@ -4,6 +4,11 @@ window.MathJax = {
         displayMath: [['$$', '$$'], ['\\[', '\\]']],
         processEscapes: true
     },
+    chtml: {
+        fontURL: chrome.runtime.getURL('lib/mathjax/output/chtml/fonts/woff-v2'),
+        scale: 1,
+        minScale: .5
+    },
     options: {
         skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre']
     }
